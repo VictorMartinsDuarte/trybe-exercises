12df4cec317d6b27f664ce31376388e741f43a74
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-23 19:21:24 -0300

    Parte de Exercícios - #5 #6

---
+++ exercises/bloco_4/4.1/exercises-js/script6.js
@@ -0,0 +1,19 @@
+let piece = "CaVaLo".toLowerCase();
+
+switch (piece) {
+    case "peao":
+        console.log(1);
+        break;
+    case "bispo":
+        console.log(2);
+        break;
+    case "torre":
+        console.log(3);
+        break;
+    case "cavalo":
+        console.log(4);
+        break;
+    default:
+        console.log("Nada");
+        break;
+}
