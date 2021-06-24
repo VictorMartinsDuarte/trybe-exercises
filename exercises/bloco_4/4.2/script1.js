85e4d414df6e731bbef0b1176ad3e1c635cff87f
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-24 20:48:26 -0300

    Exercícios - #5

--- exercises/bloco_4/4.2/script1.js
+++ exercises/bloco_4/4.2/script1.js
@@ -4,19 +4,29 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log(numbers[index]);
 // }
 
-let sumNumbers = null;
+// let sumNumbers = null;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    sumNumbers = sumNumbers + numbers[index];
-}
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sumNumbers = sumNumbers + numbers[index];
+// }
 
 // console.log(sumNumbers);
 
-let average = (sumNumbers/numbers.length);
-console.log(average);
+// let average = (sumNumbers/numbers.length);
+// console.log(average);
 
-if (average > 20) {
-    console.log("Valor maior que 20.");
-} else {
-    console.log("Valor menor ou igual a 20.");
+// if (average > 20) {
+//     console.log("Valor maior que 20.");
+// } else {
+//     console.log("Valor menor ou igual a 20.");
+// }
+
+let maiorNumber = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (maiorNumber < numbers[index]) {
+        maiorNumber = numbers[index];
+    }
 }
+
+console.log(maiorNumber);
