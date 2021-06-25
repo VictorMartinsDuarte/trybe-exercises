be4e05c09e9bd3d82df6d7f5f16ee07b2984dacf
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-24 22:29:56 -0300

    Exercícios - #1

--- exercises/bloco_4/4.2/script1.js
+++ exercises/bloco_4/4.2/script1.js
@@ -1,45 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// for (let index = 0; index < numbers.length; index += 1) {
-//     console.log(numbers[index]);
-// }
-
-// let sumNumbers = null;
-
-// for (let index = 0; index < numbers.length; index += 1) {
-//     sumNumbers = sumNumbers + numbers[index];
-// }
-
-// console.log(sumNumbers);
-
-// let average = (sumNumbers/numbers.length);
-// console.log(average);
-
-// if (average > 20) {
-//     console.log("Valor maior que 20.");
-// } else {
-//     console.log("Valor menor ou igual a 20.");
-// }
-
-// let maiorNumber = 0;
-
-// for (let index = 0; index < numbers.length; index += 1) {
-//     if (maiorNumber < numbers[index]) {
-//         maiorNumber = numbers[index];
-//     }
-// }
-
-// console.log(maiorNumber);
-
-let qntImpares = 0;
-
 for (let index = 0; index < numbers.length; index += 1) {
-    if ((numbers[index] % 2) != 0) {
-        qntImpares += 1
-    }
-}
-if (qntImpares === 0) {
-    console.log("Nenhum valor ímpar encontrado.");
-} else {
-    console.log(qntImpares);
+    console.log(numbers[index]);
 }
