4f865703266712bc070d1e55fc908a777ac31533
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-25 03:10:10 -0300

    Exercícios - #8

---
+++ exercises/bloco_4/4.2/script8.js
@@ -0,0 +1,9 @@
+// Questão: Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let result = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    result[index - 1] =  index;
+}
+
+console.log(result);
