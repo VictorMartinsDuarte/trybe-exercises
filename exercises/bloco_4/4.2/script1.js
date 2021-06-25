c6c30dfbb4c56cdf7dfc43999572a26325db3070
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-24 22:53:22 -0300

    Exercícios - #4

--- exercises/bloco_4/4.2/script1.js
+++ exercises/bloco_4/4.2/script1.js
@@ -13,4 +13,10 @@ for (let index = 0; index < numbers.length; index += 1) {
 // console.log(sumNumbers);
 
 let average = (sumNumbers/numbers.length);
-console.log(average);
+// console.log(average);
+
+if (average > 20) {
+    console.log("Valor maior que 20.");
+} else {
+    console.log("Valor menor ou igual a 20.");
+}
