4fdbc957260c7cbce503c01f4c95aa989f0ef757
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-24 22:33:50 -0300

    Exercícios - #2

--- exercises/bloco_4/4.2/script1.js
+++ exercises/bloco_4/4.2/script1.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+let sumNumbers = null;
+
 for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
+    sumNumbers = sumNumbers + numbers[index];
 }
+
+console.log(sumNumbers);
