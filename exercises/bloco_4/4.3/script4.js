2e10fc58db9107ad08a8ff76542542770622ecdb
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-27 15:41:24 -0300

    Atualizei a questão #4

--- exercises/bloco_4/4.3/script4.js
+++ exercises/bloco_4/4.3/script4.js
@@ -5,10 +5,10 @@ let n = 7;
 for (let qntLinhas = 0; qntLinhas < n; qntLinhas += 2) {
     let resultado = "";
     
-    for (let espaçosVazios = 1; espaçosVazios < (n - qntLinhas); espaçosVazios += 2) {
+    for (let espaçosVazios = 2; espaçosVazios < (n - qntLinhas); espaçosVazios += 2) {
         resultado += " ";
     }
-    for (let asteriscos = (n + qntLinhas); asteriscos > (n-1); asteriscos -= 1) {
+    for (let asteriscos = 0; asteriscos < (qntLinhas + 1); asteriscos += 1) {
         resultado += "*";
     }
     console.log(resultado);
