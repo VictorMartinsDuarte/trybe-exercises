d8f22db780c309d0c06ad49a4201c09d5e1f79c9
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-28 20:47:49 -0300

    Exercícios - #5

--- exercises/bloco_4/4.4/exPart1-1.js
+++ exercises/bloco_4/4.4/exPart1-1.js
@@ -15,3 +15,38 @@ let info = {
 // *  Valor esperado no console:
 
 info.recorrente = 'Sim';
+// info['recorrente'] = 'Sim';
+console.log(info);
+
+// 3. Faça um for/in que mostre todas as chaves do objeto.
+// * Valor esperado no console:
+
+for (let index in info) {
+    console.log(index);
+}
+
+// 4. Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+// * Valor esperado no console:
+
+for (let index in info) {
+    console.log(info[index]);
+}
+
+// 5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+// * Valor esperado no console:
+// Ambos recorrentes // Atenção para essa última linha!
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota:'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+for (let index in info, info2) {
+    if (info[index] === info2[index]) {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[index] + ' e ' + info2[index]);
+    }
+}
