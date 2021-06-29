f81cf4d8a48269395b0662b2d9940e483be1634d
Victor Martins <victor.martins.duarte@gmail.com>
2021-06-28 21:31:37 -0300

    Atualizado exercício #1 de parte de Funções

--- exercises/bloco_4/4.4/exPart2-1.js
+++ exercises/bloco_4/4.4/exPart2-1.js
@@ -1,18 +1,20 @@
-let resultado;
+// Agora vamos fazer um exercício que vai deixar claro como funções com responsabilidades bem definidas deixam o código mais bem escrito.
+// 1. Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+let a = 'Arura'.toLowerCase();
 
 function isPalindrome (a) {
     for (let index = 1; index <= a.length; index += 1) {
-        if (a[index - 1] === a[a.length - index]) {
-        } else {
-            console.log(a + ' não é um palíndromo!');
-            break;
+        if (a[index - 1] != a[a.length - index]) {
+            return ' não é um palíndromo!';
         }
-        if (false) {
-            break;
-        } else {
-            console.log(a + ' é um palíndromo!');
-        }
-    }
+    return ' é um palíndromo!'; 
+    }   
 }
 
-isPalindrome('Arara'.toLowerCase());
+console.log(a.toUpperCase() + isPalindrome(a));
+
+// 2. Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1]; .
+// Valor esperado no retorno da função: 4 .
+
