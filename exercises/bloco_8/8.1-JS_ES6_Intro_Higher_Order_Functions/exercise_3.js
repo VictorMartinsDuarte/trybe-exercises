433b14bcea450f4009e6486177b576d9cfbf6f9f
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-28 22:43:22 -0300

    Encurtando a HOF, retirando o return.

--- exercises/bloco_8/8.1-JS_ES6_Intro_Higher_Order_Functions/exercise_3.js
+++ exercises/bloco_8/8.1-JS_ES6_Intro_Higher_Order_Functions/exercise_3.js
@@ -4,9 +4,7 @@
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
-const studentGrade = (correctAnswers, verifyAnswers, checkAnswers) => {
-  return checkAnswers(correctAnswers, verifyAnswers);
-}
+const studentGrade = (correctAnswers, verifyAnswers, checkAnswers) => checkAnswers(correctAnswers, verifyAnswers);
 
 const checkAnswers = (a, b) => {
   let counter = 0;
