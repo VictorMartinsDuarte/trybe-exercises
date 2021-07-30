78e0e9fcff66e57dd93bc6d337700db8911765ab
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-30 20:09:58 -0300

    Exercício 5, e module.export nos arquivos

---
+++ exercises/bloco_8/8.3-JS_ES6_HOF-map_filter/exercise_5.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { toNamespacedPath } = require('path/posix');
+const books = require('./database');
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  return books.filter((value) => value.genre === 'Fantasia' || value.genre === 'Ficção Científica').map((value) => value.author.name).sort();
+}
+
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(),expectedResult);
+console.log(fantasyOrScienceFictionAuthors());
