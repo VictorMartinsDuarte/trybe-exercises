9747e83124b7e2333bef95b4f6dbc226a1112627
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-30 22:22:54 -0300

    Exercicio 7

---
+++ exercises/bloco_8/8.3-JS_ES6_HOF-map_filter/exercise_7.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const books = require('./database');
+
+const expectedResult = 'O Senhor dos Anéis';
+
+
+function authorWith3DotsOnName() { 
+  return books.filter((value) => value.author.name).find((element, i) => {
+    let counter = 0;
+    for (let index = 0; index < element.author.name.length; index += 1) {
+      if (element.author.name[i] === '.') {
+        counter += 1
+      }
+    }
+    if (counter > 2) {
+      return true;
+    }
+  }).name
+}
+
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
+console.log(authorWith3DotsOnName());
+
+// element.author.name.split(' ') = ['J.', 'R.', 'R.', 'Tolkien'];
+// filter((word) => word.endsWith('.')) = ['J.', 'R.', 'R.'];
