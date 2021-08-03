069a29425a930b11652c4489ce2b53461d78cc23
Victor Martins <victor.martins.duarte@gmail.com>
2021-08-03 19:32:30 -0300

    Exercício 3

--- exercises/bloco_8/8.5-JS_ES6-spread_op-param_rest-destructuring/exercise_2.js
+++ exercises/bloco_8/8.5-JS_ES6-spread_op-param_rest-destructuring/exercise_2.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 // escreva sum abaixo
-const sum = (...rest) => rest.reduce((acc, curV) => acc + curV, 0)
+const sum = (...rest) => rest.reduce(((acc, curV) => acc + curV), 0)
 
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
